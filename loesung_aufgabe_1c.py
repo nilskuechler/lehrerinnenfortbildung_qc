928cec2fa7ea67291d4f6c73e572bf99061ba0c8
nilskuechler <nilskuechler@web.de>
2023-06-07 08:57:25 +0200

    Update loesung_aufgabe_1c.py

--- loesung_aufgabe_1c.py
+++ loesung_aufgabe_1c.py
@@ -1,1 +1,33 @@
+from dwave.system import DWaveSampler
+import dwave.inspector
+
+sampler = DWaveSampler(
+    solver='Advantage_system4.1',
+)
+
+h = {
+}
+
+# Programmieren Sie hier die Werte der 
+# Koppler wie in Aufgabe 1a in der Form
+# (qubit1, qubit2): Kopplerwert,
+# Qubit 421 = Lauch
+# Qubit 466 = Mais
+# Qubit 3302 = Sellerie
+# Qubit 3347 = Erbse
+J = {
+    (421,3302): -1,
+    (421,3347): +1,
+    (466,3302): +1,
+    (466,3347): -1,
+}
+
+response = sampler.sample_ising(
+    h, 
+    J, 
+    num_reads=100,
+)
+
+dwave.inspector.show(response)
+print(response)
 
