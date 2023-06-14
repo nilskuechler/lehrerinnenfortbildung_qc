011ae4251652bdd07ad7d8b8c4e492061732e918
nilskuechler <nilskuechler@web.de>
2023-06-14 11:49:02 +0200

    Update loesung_aufgabe_2c.py

--- loesung_aufgabe_2c.py
+++ loesung_aufgabe_2c.py
@@ -9,18 +9,22 @@ h = {}
 
 J = {
     ('Lisa','Timo'): 3,
-    ('Lisa','Anna'): 2,
-    ('Lisa','Tom'): 6,
+    ('Lisa','Anna'): 5,
+    ('Lisa','Tom'): 7,
+    ('Lisa', 'Onur'): 6,
     ('Timo','Anna'): 5,
     ('Timo','Tom'): 3,
+    ('Timo','Onur'): 8,
     ('Anna','Tom'): 5,
+    ('Anna','Onur'): 2
 }
 
 response = sampler.sample_ising(
     h, 
     J, 
     num_reads=100,
-    chain_strength=4,
+    chain_strength=15,
     annealing_time=100,
 )
 dwave.inspector.show(response)
+print(response)
